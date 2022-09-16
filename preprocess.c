daf817ffb079bf4b5d5d6e05e555f102330c73bc
reyu <ka15sugar@gmail.com>
2022-09-17 04:08:43 +0900

    feat: add `#ifdef`, `#else`, `#endif`

--- preprocess.c
+++ preprocess.c
@@ -13,16 +13,18 @@ Token *preprocess(Token *tok)
 	{
 		if (t->kind == TK_PREPROCESSOR)
 		{
-			if (startswith(tok->str->ptr, "#include"))
+			if (startswith(t->str->ptr, "#include"))
 			{
 				char filename[100] = {};
 				char path[200] = {};
-				strncpy(filename, tok->str->ptr + 10, tok->str->len - 11);
+				strncpy(filename, t->str->ptr + 10, t->str->len - 11);
 
 				snprintf(path, sizeof(path), "%s/%s", dir_name, filename);
 
 				char *header = read_file(path);
 				Token *header_token = tokenize(header, false);
+				header_token = preprocess(header_token);
+				printf("// include %s\n", filename);
 				if (!header_token)
 					error("failed to tokenize %s", path);
 
@@ -36,6 +38,34 @@ Token *preprocess(Token *tok)
 					start = header_token;
 
 				before = header_token_end;
+			}
+
+			if (startswith(t->str->ptr, "#ifdef"))
+			{
+				while (!(startswith(t->str->ptr, "#else") || startswith(t->str->ptr, "#endif")))
+					t = t->next;
+
+				if (startswith(t->str->ptr, "#else"))
+				{
+					t = t->next;
+
+					if (before)
+						before->next = t;
+					else
+						start = t;
+
+					while (t->kind == TK_PREPROCESSOR && !startswith(t->str->ptr, "#endif"))
+					{
+						before = t;
+						t = t->next;
+					}
+
+					t = t->next;
+				}
+				else
+				{
+					assert(startswith(t->str->ptr, "#endif"));
+				}
 			}
 		}
 		else
